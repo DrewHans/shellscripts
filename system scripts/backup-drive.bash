1629d91ed92df6df7fb0d384b5b8b0d142c13cc5
Drew Hans <drewhans555@gmail.com>
2020-02-27 09:31:26 -0600

    Update backup-drive.bash

--- system scripts/backup-drive.bash
+++ system scripts/backup-drive.bash
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 # Author: Drew Hans (github.com/drewhans555)
 
-srcdir=/media/nyancat/MyDrive/
-dstdir=/media/nyancat/MyDriveBAK/
+srcdir=/media/nyancat/MyDrive
+dstdir=/media/nyancat/MyDriveBAK
 
-sudo cp -arux $srcdir $dstdir
+cp -arux $srcdir $dstdir
 # -a => archive -> preserves mode, ownership, and timestamps; symlinks are not dereferenced
 # -r => recursive -> copy directories recursively
 # -u => update -> copy only when a file is newer than the destination file or when the destination file is missing
-# -x => one-file-system -> stay on this file system
+# -x => one-file-system -> stay on $srcdir's file system
 
-# Note: use rsync when you want to minimize traffic on the wire; beware, rsync has a lot of overhead;
+# Note: use rsync when you want to update an existing backup and/or minimize traffic on the wire;
+#       beware, rsync has a lot of overhead and is several times slower than cp (at least 2x slower in my experience);
+
+# Create an index file for faster grepping (especially on optical media discs [CD-Rs are super slow])
+ls -R $dstdir/ >> $dstdir/MyDriveBAK.index
+
+# Search Example: grep -i 'search term' /media/nyancat/MyDriveBAK.index
