418429d2cb2b2dee46dd46b561b2bf972a5fcd6e
Drew Hans <drewhans555@gmail.com>
2020-03-04 23:04:31 -0600

    Update backup-drive.bash

--- system scripts/backup-drive.bash
+++ system scripts/backup-drive.bash
@@ -10,9 +10,6 @@ cp -arux $srcdir $dstdir
 # -u => update -> copy only when a file is newer than the destination file or when the destination file is missing
 # -x => one-file-system -> stay on $srcdir's file system
 
-# Note: use rsync when you want to update an existing backup and/or minimize traffic on the wire;
-#       beware, rsync has a lot of overhead and is several times slower than cp (at least 2x slower in my experience);
-
 # Create an index file for faster grepping (especially on optical media discs [CD-Rs are super slow])
 ls -R $dstdir/ >> $dstdir/MyDriveBAK.index
 
