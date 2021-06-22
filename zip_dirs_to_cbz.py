24bb85a7d91ec562dee0852368f72100e8e464af
Drew Hans <drewhans555@gmail.com>
2021-06-22 14:09:37 -0500

    Add files via upload

---
+++ zip_dirs_to_cbz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os, subprocess
+
+
+print("Starting.")
+
+for root, dirs, files in os.walk(".", topdown=False):
+    i = 1
+    for dname in dirs:
+        print(f"zipping {dname} ({i} of {len(dirs)})")
+        bash_command = f'zip -r ./"{dname}.cbz" ./"{dname}"'
+        output = subprocess.check_output(['bash','-c', bash_command])
+        i += 1
+
+print("Finished.")
