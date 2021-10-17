9f0889adc69249be3251dc93c0de237671a970d2
DrewHans <programmer.drew.hans@gmail.com>
2021-10-17 14:16:08 -0500

    Add script to remind myself how to use proton in terminal

---
+++ bin/run_proton.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# declare env variables required by proton
+STEAM_COMPAT_CLIENT_INSTALL_PATH=~/.var/app/com.valvesoftware.Steam/.local/share/Steam/
+STEAM_COMPAT_DATA_PATH=/mnt/gamedisk/steam/steamapps/compatdata
+
+# set path to proton python script
+PROTON=/mnt/gamedisk/steam/steamapps/common/Proton\ -\ Experimental/proton
+
+# set path to windows game's exe
+GAME_EXE=/mnt/gamedisk/freeware/GAMENAME/game.exe
+
+# launch the game with proton
+$PROTON run $GAME_EXE
