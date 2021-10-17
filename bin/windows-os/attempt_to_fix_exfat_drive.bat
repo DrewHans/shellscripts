8d62a08f33330b60d97e8fe56ee023642e13209d
DrewHans <programmer.drew.hans@gmail.com>
2021-10-16 19:27:59 -0500

    Update script

--- bin/windows-os/attempt_to_fix_exfat_drive.bat
+++ bin/windows-os/attempt_to_fix_exfat_drive.bat
@@ -1,10 +1,11 @@
 rem running chkdsk to attempt repair
-chkdsk e: /f /r /x
+chkdsk e: /f /r /x /v
 
 rem "e" means the drive letter of the partition you want to repair
-rem /f is running to fix any found errors
-rem /r is running to locate for bad sectors and recover any readable information
-rem /x is running to force the volume you're about to check to be dismounted before the utility begins a scan
+rem /f means fix any found errors
+rem /r means recover any readable information from bad sectors
+rem /x means volume will be dismounted before the utility begins a scan
+rem /v means verbose (iI think)
 
 rem running system file checker tool to repair missing or corrupted system files
 sfc /scannow
