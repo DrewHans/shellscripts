e593be97228add795c2627073cfc333ab0611935
DrewHans <programmer.drew.hans@gmail.com>
2021-12-14 21:47:58 -0600

    update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,16 +6,19 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit 1
 fi
 
+echo "Starting $0"
+
 src_path="/media/veracrypt1"  # should be master drive
 dest_path="/media/veracrypt2"  # should be backup drive
 
 echo "Regenerating master.index file"
+
 ls -R $src_path > $src_path/master.index
 
-echo ""
-sudo rsync -r -t -x -v --progress --delete -i -s $src_path/ $dest_path
+echo "Starting rsync"
 
-# options:
+sudo rsync -r -t -x -v --progress --delete -i -s $src_path/ $dest_path | tee -a /tmp/rsync_output.log
+# rsync options:
 #  -r => recurse into directories
 #  -t => preserve modification times
 #  -x => don't cross filesystem boundaries
@@ -23,3 +26,5 @@ sudo rsync -r -t -x -v --progress --delete -i -s $src_path/ $dest_path
 #  --progress => show progress during tansfer
 #  -i => output a change-summary for all updates
 #  -s => no space-splitting; wildcard chars only
+
+echo "$0 finished"
