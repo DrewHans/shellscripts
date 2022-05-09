6f576dd396d2ec4dc3123bd7724a90f399bd9998
DrewHans <programmer.drew.hans@gmail.com>
2022-05-08 19:51:42 -0500

    Update bitrate

--- bin/convert-webm-to-mp3.sh
+++ bin/convert-webm-to-mp3.sh
@@ -7,4 +7,4 @@ command -v ffmpeg >/dev/null 2>&1 || {
     exit 1
 }
 
-find . -type f -iname "*.webm" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -ab 196k -ar 44100 -y "${FILE%.webm}.mp3";' _ '{}' \;
+find . -type f -iname "*.webm" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -ab 128k -ar 44100 -y "${FILE%.webm}.mp3";' _ '{}' \;
