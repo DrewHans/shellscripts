506eb80409820383999b0fa0e24ce7852ed6a3f4
DrewHans <programmer.drew.hans@gmail.com>
2022-05-08 19:45:56 -0500

    Add helper script for webm audio conversion

---
+++ bin/convert-webm-to-mp3.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+# check prerequisite program ffmpeg is installed
+command -v ffmpeg >/dev/null 2>&1 || {
+    echo "ffmpeg program not found; aborting"
+    exit 1
+}
+
+find . -type f -iname "*.webm" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -ab 196k -ar 44100 -y "${FILE%.webm}.mp3";' _ '{}' \;
