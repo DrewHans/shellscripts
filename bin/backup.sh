97937ff68834cc2f1ff0f28c66d6b4489f8b9652
DrewHans <programmer.drew.hans@gmail.com>
2022-07-13 16:03:37 -0500

    Update drive backup.sh script

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-# exit if not running as root
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Error: You must run this script as root"
+# exit if running as root
+if [[ $(/usr/bin/id -u) -eq 0 ]]; then
+    echo "Error: Do not run this script as root"
     exit 1
 fi
 
@@ -17,6 +17,7 @@ echo "Starting $0"
 
 src_path="/media/veracrypt1"  # should be master drive
 dest_path="/media/veracrypt2"  # should be backup drive
+log_path="/home/nyancat"  # should be user's home dir
 
 if [[ ! -d "$src_path" ]]
 then
@@ -34,15 +35,16 @@ echo "Regenerating master.index file"
 
 ls -R $src_path > $src_path/master.index
 
-echo "Starting rsync"
+echo "Starting rsync (check $log_path for log output)"
 
-sudo rsync -r -t -x -v --progress --delete -i -s $src_path/ $dest_path | tee -a /tmp/rsync_output.log
+rsync -r -t -x -v --progress --delete -i -s $src_path/ $dest_path | tee -a $log_path/rsync_output.log
 # rsync options:
 #  -r => recurse into directories
 #  -t => preserve modification times
 #  -x => don't cross filesystem boundaries
 #  -v => increases verbosity
 #  --progress => show progress during tansfer
+#  --delete => delete extraneous files from dest dirs
 #  -i => output a change-summary for all updates
 #  -s => no space-splitting; wildcard chars only
 
