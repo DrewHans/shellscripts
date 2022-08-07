d56c038e25af8e9c9ab97131ea4b21ff2ee144bc
DrewHans <programmer.drew.hans@gmail.com>
2022-08-07 10:14:50 -0500

    Update script to dl images

--- bin/dl-images.sh
+++ bin/dl-images.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
 
-# check prerequisite program gallery-dl is installed
-command -v gallery-dl >/dev/null 2>&1 || {
-    echo "gallery-dl program not found; aborting"
-    exit 1
-}
+if [ "$#" -ne 1 ]; then
+	echo "Error: you must pass in a website url"
+	exit 1
+fi
 
-start_index=1
-end_index=99
+website_url="$1"
+save_location="images"
 
-for i in {$start_index..$end_index}; do
-    url="https://some-website.com/g/some-path/$i"
-    gallery-dl "r:$url";
-done
+wget \
+	--level=1 \
+	--no-directories \
+	--recursive \
+	--directory-prefix=$save_location \
+	--accept "*.jpg, *.jpeg, *.png, *.webp" \
+	$website_url
+# --level=1 => only look at the first subdirectory (the page with the images)
+# --no-directories => do not create a hierarchy of directories when retrieving recursively
+# --recursive => turn on recursive retrieving
+# --directory-prefix=images => the directory where all downloaded files will be saved
+# --accept "*.jpg, *.jpeg, *.png" => accept only files with these extensions
