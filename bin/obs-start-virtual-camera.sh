3826551618609153570aa5f62a0baa241acf20e3
DrewHans <programmer.drew.hans@gmail.com>
2022-08-22 16:31:03 -0500

    Fix typo in script

--- bin/obs-start-virtual-camera.sh
+++ bin/obs-start-virtual-camera.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-# exit if running as root
+# exit if not running as root
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "Error: Do you must run this script as root"
+    echo "Error: You must run this script as root"
     exit 1
 fi
 
