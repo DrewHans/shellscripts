77239d90f84e0b7406e223da65136cdcba56df67
DrewHans <programmer.drew.hans@gmail.com>
2022-12-13 17:42:41 -0600

    Fix condition typo in update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -12,19 +12,25 @@ function check_is_root {
 # safety checks
 check_is_root
 
-if ! command -v "apt" > /dev/null 2>&1
+if command -v "apt" > /dev/null 2>&1
 then
 	apt update && apt upgrade --yes
+else
+	echo "apt is not installed; skipping 'apt update && apt upgrade --yes'"
 fi
 
-if ! command -v "flatpak" > /dev/null 2>&1
+if command -v "flatpak" > /dev/null 2>&1
 then
 	sudo -u ${SUDO_USER} flatpak update --assumeyes
+else
+	echo "flatpak is not installed; skipping 'flatpak update --assumeyes'"
 fi
 
-if ! command -v "yt-dlp" > /dev/null 2>&1
+if command -v "yt-dlp" > /dev/null 2>&1
 then
 	yt-dlp --update
+else
+	echo "yt-dlp is not installed; skipping 'yt-dlp --update'"
 fi
 
 echo "$0 finished"
