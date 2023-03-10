6e56e46a558c1077c9631afb98c6ff63fcfa02ff
DrewHans <programmer.drew.hans@gmail.com>
2023-03-09 19:22:19 -0600

    Add reminder for wiiu file conversion

---
+++ bin/convert-wiiu-files-to-three-folders.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Don't run this script! Read file instead."
+exit 1
+
+
+# Step 1. download and build cdecrypt
+
+# Step 2. run cdecrypt <folder_path> to convert to three folders
+
+# Step 3. repeat for DLC and update folders
+
+# Step 4. test with CEMU to verify that everything was converted correctly
