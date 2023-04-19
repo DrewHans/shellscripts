171cf3f814ea5e6d22440c94f38e1edb7f57000c
DrewHans <programmer.drew.hans@gmail.com>
2023-04-18 20:05:30 -0500

    Add antivirus-scan.sh & update permissions

---
+++ bin/antivirus-scan.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+function check_dependency {
+	if ! command -v "$1" > /dev/null 2>&1
+	then
+		echo "This script requires $1 to be installed."
+		echo "Please use your distribution's package manager to install it."
+		exit 2
+	fi
+}
+
+function check_is_root {
+	if [[ $EUID -ne 0 ]]
+	then
+		echo "This script must be run as root."
+		exit 1
+	fi
+}
+
+# safety checks
+check_is_root
+check_dependency "clamscan"
+
+clamscan -r -i -v /
+# -r => scan directories recursively
+# -i => only print infected files
+# / => the path to start searching in
+
+echo "$0 finished"
